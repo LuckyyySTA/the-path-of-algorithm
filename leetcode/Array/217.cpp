016aad008729fdfe8881321bee04f235937c6116
codehuanglei <1142372633@qq.com>
2020-12-13 11:30:41 +0800

    12.13 commit

---
+++ leetcode/Array/217.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> record;
+        for(int i = 0; i < nums.size(); i++){
+          if(record.find(nums[i]) == record.end()){
+            record.insert(nums[i]);
+          }
+          else{
+            return true;
+          }
+        }
+        return false;
+    }
+};
+
+void print_bool(bool res){
+  cout<<(res ? "True" : "False")<<endl;
+}
+
+int main(){
+  vector<int> vec = {0, 0, 1};
+  print_bool(Solution().containsDuplicate(vec));
+  return 0;
+}
