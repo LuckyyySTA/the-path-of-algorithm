a52b7df4d897c4c57327b67cfd01b6e2bb8ab623
codehuanglei <1142372633@qq.com>
2020-12-14 01:24:57 +0800

    12.14 commit

--- leetcode/Hashmap/49.cpp
+++ leetcode/Hashmap/49.cpp
@@ -1,16 +1,36 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        unordered_map<string,vector<string>> m;
-        for(string& s : strs){
-            string t = s;
-            sort(t.begin(), t.end());
-            m[t].push_back(s);
+        unordered_map<string,vector<string>> map;
+        for(string s : strs){
+            string key = s;
+            sort(key.begin(),key.end());
+            map[key].push_back(s);
         }
-        for(auto& n : m){
-            res.push_back(n.second);
+        //  想要拷贝元素：for(auto x : range)
+       //   想要修改元素 :  for(auto& x : range)
+       //   想要只读元素：for(const auto& x : range) 
+        for(const auto& p : map){
+            res.push_back(p.second);
         }
         return res;
     }
 };
+void print_vec(vector<vector<string>>& s){
+    for(vector<string> vec : s){
+        for(string c : vec){
+            cout<<c<<' ';
+        }
+        cout<<endl;
+    }
+}
+int main(){
+
+}
