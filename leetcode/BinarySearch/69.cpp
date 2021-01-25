90ff44a483b1b1667a2ef0c78c808a86729b55ca
codehuanglei <1142372633@qq.com>
2021-01-25 17:52:27 +0800

    1.25 commit

--- leetcode/BinarySearch/69.cpp
+++ leetcode/BinarySearch/69.cpp
@@ -3,24 +3,22 @@ using namespace std;
 class Solution {
 public:
     int mySqrt(int x) {
-      if(x == 1){
-        return 1;
-      }
-      int left = 0, right = x;
-      while(left <= right){
-        long long  mid = left + (right - left)/2;
-        if(mid*mid == x){
-          return mid;
-        }else if(mid*mid < x){
-          if((mid+1)*(mid+1) > x){
-            return mid;
-          }
-          left = mid + 1;
-        }else{
-          right = mid - 1;
+        int left = 1, right = x;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(mid > x/mid){
+                right = mid - 1;
+            }else if(mid < x/mid){
+                if((mid + 1) > x/(mid + 1)){
+                    return mid;
+                }
+                left = mid + 1;
+            }
+            else{
+                return mid;
+            }
         }
-      }
-      return -1;
+        return 0;
     }
 };
 int main(){
