bede245676b1344a68c61acd2222b2a6ff0b1d06
codehuanglei <1142372633@qq.com>
2021-01-25 18:16:07 +0800

    1.25 commit

--- leetcode/BinarySearch/278.cpp
+++ leetcode/BinarySearch/278.cpp
@@ -1,21 +1,18 @@
-#include<iostream>
-using namespace std;
 // The API isBadVersion is defined for you.
-// bool isBadVersion(int version)#
+// bool isBadVersion(int version);
+
 class Solution {
 public:
     int firstBadVersion(int n) {
         int left = 1, right = n;
-        while(left<right){ // 1 2 3 
-          int mid = left + (right - left)/2;
-          if(isBadVersion(mid)){
-            // [left, mid]
-            right = mid;
-          }else{
-            // [mid+1,right]
-            left = mid + 1;
-          }
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(isBadVersion(mid) == false){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
         }
-        return left;
+        return isBadVersion(left) == true ? left : -1;
     }
 };
