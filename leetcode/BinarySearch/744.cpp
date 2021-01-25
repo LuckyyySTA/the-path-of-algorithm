ea6179a2bfde8ccd44aa81edbd5e520290d828fe
codehuanglei <1142372633@qq.com>
2021-01-25 20:51:59 +0800

    1.25 commit

--- leetcode/BinarySearch/744.cpp
+++ leetcode/BinarySearch/744.cpp
@@ -7,16 +7,14 @@ public:
     char nextGreatestLetter(vector<char>& letters, char target) {
       int left = 0, right = letters.size() - 1;
       while(left < right){
-        int mid = left + (right - left)/2;
-        if(letters[mid] > target){
-          // [left, mid]
-          right = mid;
-        }else{
-          // [mid + 1, right]
-          left = mid + 1;
-        }
+          int mid = left + (right - left)/2;
+          if(letters[mid] <= target){
+              left = mid + 1;
+          }else{
+              right = mid;
+          } 
       }
-      return letters[left] <= target ? letters[0] : letters[left];
+      return letters[left] > target ? letters[left] : letters[0];
     }
 };
 int main(){
