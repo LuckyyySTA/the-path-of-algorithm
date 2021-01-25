24be785bbdb80a00ac51e562d8e816f7adbfc2c4
codehuanglei <1142372633@qq.com>
2021-01-25 16:52:29 +0800

    1.25 commit

--- leetcode/BinarySearch/704.cpp
+++ leetcode/BinarySearch/704.cpp
@@ -12,9 +12,9 @@ public:
           return mid;
         }
         else if(nums[mid] < target){
-          right = mid - 1;
-        }else{
           left = mid + 1;
+        }else{
+          right = mid - 1;
         }
       }
       return -1;
