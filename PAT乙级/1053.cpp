3ed8153d9a3dcd3edb795b11511c793985900750
codehuanglei <1142372633@qq.com>
2021-01-31 11:00:07 +0800

    1.31 commit

---
+++ PAT乙级/1053.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N, D;
+    float e;
+    cin>>N >> e >> D;
+    double maybeEmpty = 0, Empty = 0;
+    int num = N;
+    while(N--){
+        int k;
+        cin>>k;
+        int count = 0;
+        for(int i = 0; i < k; i++){
+            float E;
+            cin>>E;
+            if(E < e){
+                count++;
+            }
+        }
+        if(count > k/2){
+            maybeEmpty++;
+        }
+        if(count > k/2 && k > D){
+            Empty++;
+        }
+    }
+    cout<<maybeEmpty<<" "<<Empty<< " "<<num<<endl;
+    printf("%.1f%% %.1f%%", ((maybeEmpty-Empty)*100)/num , (Empty*100)/num);
+    return 0;
+}
