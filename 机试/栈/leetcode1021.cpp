be89b012a5a6defcccfcef5092298bcdbfa31c9c
codehuanglei <1142372633@qq.com>
2021-02-19 12:16:31 +0800

    2.19 commit

---
+++ 机试/栈/leetcode1021.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        int start = 0, stack = 1;
+        string res = "";
+        for(int i = 1; i < S.length(); i++){
+            if(S[i] == '('){
+                stack++;
+            }else{
+                stack--;
+            }
+            if(stack == 0){
+                res += S.substr(start + 1, i - start - 1);
+                start = i + 1;
+            }
+        }
+        return res;
+    }
+};
