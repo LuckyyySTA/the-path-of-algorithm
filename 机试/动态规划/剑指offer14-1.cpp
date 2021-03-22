67986d145e16661462a8c03fea4bfe75277c4194
codehuanglei <1142372633@qq.com>
2021-03-22 13:42:45 +0800

    3.22 commit

---
+++ 机试/动态规划/剑指offer14-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int cuttingRope(int n) {
+        vector<int> dp(n+1);
+        dp[2] = 1;
+        for(int i = 3; i < n+1; i++){
+            for(int j = 2; j < i; j++){
+                dp[i] = max(dp[i], max(j*dp[i-j], j*(i - j)));
+            }
+        }
+        return dp[n];
+    }
+};
